ad05dd863ac4769d3f78cd395dff55d54a2da163
seve <severine.legon@nology.io>
2020-06-08 18:23:20 +0100

    Random coins are generated when page is refreshed. They do not spill outside the window. Style of the coins still need improvement.

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
-// DECLARE VARIABLES
-//const mario = document.getElementsByTagName('div');
 
-let block = document.getElementById('mario');
-let modifier = 4;
-console.log(block);
-// Need to figure out how to get person to move left and right of screen using 
-//keyboad arrows and can't get out of the body
+
+
+let block = document.getElementById('mario'); // GRAB MARIO
+let modifier = 4; // HOW MANY PX DOES MARIO MOVE PER PRESS
+
+// LEFT/RIGHT INITIAL POSITION OF MARIO
 block.style.top = 0;
 block.style.left = 0;
 
 
+// FUNCTION TO MAKE MARIO MOVE USING KEYS
+
 window.addEventListener("keydown", (event) => {
     const style = block.style;
-    switch(event.key) {
-        case 'ArrowUp' : 
-        return style.top = `${parseInt(style.top) - modifier}px`;
-        case 'ArrowDown' : 
-        style.top = `${parseInt(style.top) + modifier}px`;
-        break;
-        case 'ArrowLeft' : 
-        style.left = `${parseInt(style.left) - modifier}px`;
-        break;
-        case 'ArrowRight' : 
-        style.left = `${parseInt(style.left) + modifier}px`;
-        break;
+    switch (event.key) {
+        case 'ArrowUp':
+            return style.top = `${parseInt(style.top) - modifier}px`;
+        case 'ArrowDown':
+            style.top = `${parseInt(style.top) + modifier}px`;
+            break;
+        case 'ArrowLeft':
+            style.left = `${parseInt(style.left) - modifier}px`;
+            break;
+        case 'ArrowRight':
+            style.left = `${parseInt(style.left) + modifier}px`;
+            break;
     }
-
 });
+
+// GENERATE  COIN 
+
+$(document).ready(function() {
+    // get the width and height of the window
+    var width = $(window).width();
+    var height = $(window).height();
+    // add 100 divs to the page
+    for (var i = 0; i < 9; i++) {
+        // the left position of the div should be at most the width of the
+        // window minus the width of the div, so the div doesn't spill out
+        // of the right side of the window.
+        var left = Math.floor(Math.random() * (width-100));
+        // the top position of the div should be at most the height of the
+        // window minus the width of the div, so the div doesn't spill out
+        // of the bottom of the window.
+        var top = Math.floor(Math.random() * (height-100));
+        // create a new div, append it to the body and set the top and
+        // left positions in the CSS
+        $("<div></div>").appendTo("body").css({
+            left: left,
+            top: top
+        });
+    }
+});
+
+// HOW TO COLLIDE
 
 
 
